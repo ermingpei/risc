1aec43ca059285a023cfb496d65a3002ef82d267
erming <erming.pei@westdri.ca>
2023-06-13 20:49:34 +0000

    RISC v1.0.1

--- examples/else/cephfs/deploy.sh
+++
@@ -1,9 +0,0 @@
-
-ka -f secret.yaml
-
-#For static or preprovisioned cephfs volume or manila share
-ka -f nginx-cephfs-staticpv.yaml
-
-#For dynamic cephfs volume
-ka -f storageclass.yaml
-ka -f nginx-cephfs.yaml (TBD) 
